c829aff43c8b2ea63c8997138702f4c8adc4f45b
AlexandrNovizov <126908706+AlexandrNovizov@users.noreply.github.com>
2026-04-06 17:20:06 +0400

    изменен скрипт заполнения БД для многократного запуска

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,76 @@
 INSERT INTO status (status_name)
-VALUES ('ACCEPTED'), ('PENDING'), ('REJECTED'), ('DELETED');
+SELECT 'ACCEPTED'
+FROM DUAL
+WHERE (SELECT COUNT(*) FROM status) = 1;
+
+INSERT INTO status (status_name)
+SELECT 'PENDING'
+FROM DUAL
+WHERE (SELECT COUNT(*) FROM status) = 2;
+
+INSERT INTO status (status_name)
+SELECT 'REJECTED'
+FROM DUAL
+WHERE (SELECT COUNT(*) FROM status) = 3;
+
+INSERT INTO status (status_name)
+SELECT 'DELETED'
+FROM DUAL
+WHERE (SELECT COUNT(*) FROM status) = 4;
+
 
 INSERT INTO rating (rating_name)
-VALUES ('G'), ('PG'), ('PG-13'), ('R'), ('NC-17');
+SELECT 'G'
+FROM DUAL
+WHERE (SELECT COUNT(*) FROM rating) = 1;
+
+INSERT INTO rating (rating_name)
+SELECT 'PG'
+FROM DUAL
+WHERE (SELECT COUNT(*) FROM rating) = 2;
+
+INSERT INTO rating (rating_name)
+SELECT 'PG-13'
+FROM DUAL
+WHERE (SELECT COUNT(*) FROM rating) = 3;
+
+INSERT INTO rating (rating_name)
+SELECT 'R'
+FROM DUAL
+WHERE (SELECT COUNT(*) FROM rating) = 4;
+
+INSERT INTO rating (rating_name)
+SELECT 'NC-17'
+FROM DUAL
+WHERE (SELECT COUNT(*) FROM rating) = 5;
+
 
 INSERT INTO genre (genre_name)
-VALUES ('Комедия'), ('Драма'), ('Мультфильм'), ('Триллер'), ('Документальный'), ('Боевик');
+SELECT 'Комедия'
+FROM DUAL
+WHERE (SELECT COUNT(*) FROM genre) = 1;
+
+INSERT INTO genre (genre_name)
+SELECT 'Драма'
+FROM DUAL
+WHERE (SELECT COUNT(*) FROM genre) = 2;
+
+INSERT INTO genre (genre_name)
+SELECT 'Мультфильм'
+FROM DUAL
+WHERE (SELECT COUNT(*) FROM genre) = 3;
+
+INSERT INTO genre (genre_name)
+SELECT 'Триллер'
+FROM DUAL
+WHERE (SELECT COUNT(*) FROM genre) = 4;
+
+INSERT INTO genre (genre_name)
+SELECT 'Документальный'
+FROM DUAL
+WHERE (SELECT COUNT(*) FROM genre) = 5;
+
+INSERT INTO genre (genre_name)
+SELECT 'Боевик'
+FROM DUAL
+WHERE (SELECT COUNT(*) FROM genre) = 6;
